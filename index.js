ddbcdd0aafcca07498f64b2405c783cee830636b
Mathias Benjaminsen <mathias.benjaminsen@formue.no>
2024-09-09 22:23:10 +0200

    Revert "Make results return empty array"

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const request = require("request");
-const parseString = require("xml2js-parser").parseString;
-const processors = require("xml2js/lib/processors");
+const request = require('request');
+const parseString = require('xml2js-parser').parseString;
+const processors = require('xml2js/lib/processors');
 
-function makeArray(obj) {
-  return !Array.isArray(obj) ? [obj] : obj;
-}
+
 
 function EventorApi(config) {
-  this.config = config;
+    this.config = config;
 }
 
 EventorApi.DISCIPLINE_ORIEENTERING = 1;
@@ -25,389 +23,232 @@ EventorApi.CLASSTYPE_COMPETITION = 17;
 EventorApi.CLASSTYPE_DEVELOPMENT = 18;
 EventorApi.CLASSTYPE_OPEN = 19;
 
-EventorApi.prototype.options = function (path, qs) {
-  let url = this.config.eventorApiUrl + path;
-  if (qs) url += `?${qs}`;
 
-  return {
-    url: url,
-    options: {
-      method: "GET",
-      mode: "no-cors",
-      credentials: "include",
-      headers: {
-        ApiKey: this.config.apiKey,
-      },
-    },
-  };
-};
 
-EventorApi.prototype.events = async function (queryParams) {
-  const qs = new URLSearchParams(queryParams);
-  const options = this.options("events", qs);
-
-  try {
-    const response = await fetch(options.url, options.options);
-    const body = await response.text();
-    return new Promise((resolve, reject) => {
-      parseString(
-        body,
-        {
-          explicitArray: false,
-          ignoreAttrs: false,
-          mergeAttrs: true,
-          tagNameProcessors: [processors.firstCharLowerCase],
-        },
-        (err, result) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          if (result && result.eventList && result.eventList.event) {
-            let events = !Array.isArray(result.eventList.event)
-              ? [result.eventList.event]
-              : result.eventList.event;
-            resolve(events);
-          } else {
-            resolve([]);
-          }
+EventorApi.prototype.options = function(path) {
+    return {
+        url: this.config.eventorApiUrl + path,
+        headers: {
+            'ApiKey': this.config.apiKey
         }
-      );
-    });
-  } catch (err) {
-    console.error(err);
-  }
+    };
 };
 
-EventorApi.prototype.entries = async function (queryParams) {
-  const qs = new URLSearchParams(queryParams);
-  const options = this.options("entries", qs);
-
-  try {
-    const response = await fetch(options.url, options.options);
-    const body = await response.text();
-    return new Promise((resolve, reject) => {
-      parseString(
-        body,
-        {
-          explicitArray: false,
-          ignoreAttrs: false,
-          mergeAttrs: true,
-          tagNameProcessors: [processors.firstCharLowerCase],
-        },
-        (err, result) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          if (result && result.entryList && result.entryList.entry) {
-            resolve(result.entryList.entry);
-          } else {
-            resolve([]);
-          }
-        }
-      );
-    });
-  } catch (err) {
-    console.error(err);
-  }
+makeArray = function(obj) {
+    return !Array.isArray(obj) ? [obj] : obj;
 };
 
-EventorApi.prototype.competitorCount = async function (
-  organisationIds,
-  eventIds
-) {
-  const qs = new URLSearchParams({
-    organisationIds: organisationIds.join(),
-    eventIds: eventIds.join(),
-  });
-  const options = this.options("competitorcount", qs);
+EventorApi.prototype.events = function(queryParams) {
+    const options = this.options('events');
+    options.qs = queryParams;
 
-  try {
-    const response = await fetch(options.url, options.options);
-    const body = await response.text();
-    return new Promise((resolve, reject) => {
-      parseString(
-        body,
-        {
-          explicitArray: false,
-          ignoreAttrs: false,
-          mergeAttrs: true,
-          tagNameProcessors: [processors.firstCharLowerCase],
-        },
-        (err, result) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          if (
-            result &&
-            result.competitorCountList &&
-            result.competitorCountList.competitorCount
-          ) {
-            resolve(makeArray(result.competitorCountList.competitorCount));
-          } else {
-            resolve([]);
-          }
-        }
-      );
+    return new Promise(function(resolve, reject) {
+        request(options, (err, res, body) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            parseString(body, {
+                explicitArray: false,
+                ignoreAttrs: false,
+                mergeAttrs: true,
+                tagNameProcessors: [processors.firstCharLowerCase]
+            }, (err, result) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            if (result && result.eventList && result.eventList.event) {
+                let events =  (!Array.isArray(result.eventList.event)) ? [result.eventList.event] : result.eventList.event;
+                resolve(events);
+            } else {
+                resolve([]);
+            }
+
+        });
     });
-  } catch (err) {
-    console.error(err);
-  }
+    });
 };
 
-EventorApi.prototype.starts = async function (organisationId, eventId) {
-  const qs = new URLSearchParams({
-    organisationIds: organisationId,
-    eventId: eventId,
-  });
-  const options = this.options("starts/organisation", qs);
+EventorApi.prototype.entries = function(queryParams) {
+    const options = this.options('entries');
+    options.qs = queryParams;
 
-  try {
-    const response = await fetch(options.url, options.options);
-    const body = await response.text();
-    return new Promise((resolve, reject) => {
-      parseString(
-        body,
-        {
-          explicitArray: false,
-          ignoreAttrs: false,
-          mergeAttrs: true,
-          tagNameProcessors: [processors.firstCharLowerCase],
-        },
-        (err, result) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          if (result && result.startList && result.startList.classStart) {
-            resolve(makeArray(result.startList.classStart));
-          } else {
-            resolve([]);
-          }
-        }
-      );
+    return new Promise(function(resolve, reject) {
+        request(options, (err, res, body) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            parseString(body, {
+                explicitArray: false,
+                ignoreAttrs: false,
+                mergeAttrs: true,
+                tagNameProcessors: [processors.firstCharLowerCase]
+            }, (err, result) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            if (result && result.entryList && result.entryList.entry) {
+                resolve(result.entryList.entry);
+            } else {
+                resolve([]);
+            }
+
+        });
     });
-  } catch (err) {
-    console.error(err);
-  }
+    });
 };
 
-EventorApi.prototype.orgResults = async function (organisationId, eventId) {
-  const qs = new URLSearchParams({
-    organisationIds: organisationId,
-    eventId: eventId,
-  });
-  const options = this.options("results/organisation", qs);
+EventorApi.prototype.competitorCount = function(organisationIds, eventIds) {
+    const options = this.options('competitorcount');
+    options.qs = {organisationIds: organisationIds.join(), eventIds: eventIds.join()};
 
-  try {
-    const response = await fetch(options.url, options.options);
-    const body = await response.text();
-    return new Promise((resolve, reject) => {
-      parseString(
-        body,
-        {
-          explicitArray: false,
-          ignoreAttrs: false,
-          mergeAttrs: true,
-          tagNameProcessors: [processors.firstCharLowerCase],
-        },
-        (err, result) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          if (result && result.resultList && result.resultList.classResult) {
-            resolve(makeArray(result.resultList.classResult));
-          } else {
-            resolve([]);
-          }
-        }
-      );
+    return new Promise(function(resolve, reject) {
+        request(options, (err, res, body) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            parseString(body, {
+                explicitArray: false,
+                ignoreAttrs: false,
+                mergeAttrs: true,
+                tagNameProcessors: [processors.firstCharLowerCase]
+            }, (err, result) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            if (result && result.competitorCountList && result.competitorCountList.competitorCount) {
+                resolve(makeArray(result.competitorCountList.competitorCount));
+            } else {
+                resolve([]);
+            }
+
+        });
     });
-  } catch (err) {
-    console.error(err);
-  }
+    });
 };
 
-EventorApi.prototype.results = async function (queryParams) {
-  const qs = new URLSearchParams(queryParams);
-  const options = this.options("results/event", qs);
 
-  try {
-    const response = await fetch(options.url, options.options);
-    const body = await response.text();
-    return new Promise((resolve, reject) => {
-      parseString(
-        body,
-        {
-          explicitArray: false,
-          ignoreAttrs: false,
-          mergeAttrs: true,
-          tagNameProcessors: [processors.firstCharLowerCase],
-        },
-        (err, result) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          if (result && result.resultList && result.resultList.classResult) {
-            resolve(makeArray(result.resultList.classResult));
-          } else {
-            resolve([]);
-          }
-        }
-      );
+
+EventorApi.prototype.starts = function(organisationId, eventId) {
+    const options = this.options('starts/organisation');
+    options.qs = {organisationIds: organisationId, eventId: eventId};
+
+    return new Promise(function(resolve, reject) {
+        request(options, (err, res, body) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            parseString(body, {
+                explicitArray: false,
+                ignoreAttrs: false,
+                mergeAttrs: true,
+                tagNameProcessors: [processors.firstCharLowerCase]
+            }, (err, result) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            if (result && result.startList && result.startList.classStart) {
+                resolve(makeArray(result.startList.classStart));
+            } else {
+                resolve([]);
+            }
+
+        });
     });
-  } catch (err) {
-    console.error(err);
-  }
+    });
 };
 
-EventorApi.prototype.personsByOrg = async function (organisationId) {
-  const options = this.options("persons/organisations/" + organisationId);
 
-  try {
-    const response = await fetch(options.url, options.options);
-    const body = await response.text();
-    return new Promise((resolve, reject) => {
-      parseString(
-        body,
-        {
-          explicitArray: false,
-          ignoreAttrs: true,
-          tagNameProcessors: [processors.firstCharLowerCase],
-        },
-        (err, result) => {
-          if (err) {
-            reject(err);
-            return;
-          }
 
-          resolve(result);
-        }
-      );
+EventorApi.prototype.results = function(organisationId, eventId) {
+    const options = this.options('results/organisation');
+    options.qs = {organisationIds: organisationId, eventId: eventId};
+
+    return new Promise(function(resolve, reject) {
+        request(options, (err, res, body) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            parseString(body, {
+                explicitArray: false,
+                ignoreAttrs: false,
+                mergeAttrs: true,
+                tagNameProcessors: [processors.firstCharLowerCase]
+            }, (err, result) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            if (result && result.resultList && result.resultList.classResult) {
+                resolve(makeArray(result.resultList.classResult));
+
+            } else {
+                resolve([]);
+            }
+
+        });
     });
-  } catch (err) {
-    console.error(err);
-  }
+    });
 };
 
-EventorApi.prototype.person = async function (personId) {
-  const options = this.options("competitor/" + personId);
+EventorApi.prototype.persons = function(organisationId) {
+    const options =this.options('persons/organisations/' + organisationId);
 
-  try {
-    const response = await fetch(options.url, options.options);
-    const body = await response.text();
-    return new Promise((resolve, reject) => {
-      parseString(
-        body,
-        {
-          explicitArray: false,
-          ignoreAttrs: true,
-          tagNameProcessors: [processors.firstCharLowerCase],
-        },
-        (err, result) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          resolve(result);
-        }
-      );
+    return new Promise(function(resolve, reject) {
+        request(options, (err, res, body) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            console.log(body);
+            parseString(body, {
+                explicitArray: false,
+                ignoreAttrs: true,
+                tagNameProcessors: [processors.firstCharLowerCase]
+            }, (err, result) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(result.personList.person);
+
+        });
     });
-  } catch (err) {
-    console.error(err);
-  }
+    });
 };
 
-EventorApi.prototype.organisation = async function (organisationId) {
-  const options = this.options("organisation/" + organisationId);
+EventorApi.prototype.eventClasses = function(eventId) {
+    const options = this.options('eventclasses');
+    options.qs = {eventId: eventId};
+    return new Promise(function(resolve, reject) {
+        request(options, (err, res, body) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            parseString(body, {
+                explicitArray: false,
+                ignoreAttrs: true,
+                tagNameProcessors: [processors.firstCharLowerCase]
+            }, (err, result) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(result.eventClassList.eventClass);
 
-  try {
-    const response = await fetch(options.url, options.options);
-    const body = await response.text();
-    return new Promise((resolve, reject) => {
-      parseString(
-        body,
-        {
-          explicitArray: false,
-          ignoreAttrs: true,
-          tagNameProcessors: [processors.firstCharLowerCase],
-        },
-        (err, result) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          resolve(result.organisation);
-        }
-      );
+            });
+        });
     });
-  } catch (err) {
-    console.error(err);
-  }
 };
 
-EventorApi.prototype.organisations = async function () {
-  const options = this.options("organisations");
-
-  try {
-    const response = await fetch(options.url, options.options);
-    const body = await response.text();
-    return new Promise((resolve, reject) => {
-      parseString(
-        body,
-        {
-          explicitArray: false,
-          ignoreAttrs: true,
-          tagNameProcessors: [processors.firstCharLowerCase],
-        },
-        (err, result) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          resolve(result.organisationList.organisation);
-        }
-      );
-    });
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-EventorApi.prototype.eventClasses = async function (eventId) {
-  const options = this.options("eventclasses");
-  options.qs = { eventId: eventId };
-
-  try {
-    const response = await fetch(
-      options.url + "?" + new URLSearchParams(options.qs),
-      options.options
-    );
-    const body = await response.text();
-    return new Promise((resolve, reject) => {
-      parseString(
-        body,
-        {
-          explicitArray: false,
-          ignoreAttrs: true,
-          tagNameProcessors: [processors.firstCharLowerCase],
-        },
-        (err, result) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          resolve(result.eventClassList.eventClass);
-        }
-      );
-    });
-  } catch (err) {
-    console.error(err);
-  }
-};
 
 module.exports = EventorApi;
